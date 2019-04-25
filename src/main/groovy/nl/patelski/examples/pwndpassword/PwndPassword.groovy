b01032222977f89021d948a54186208788398e1f
Bram Patelski <bram.patelski@gmail.com>
2019-04-25 12:03:44 +0200

    Extract checkBySHA1 code, so we can also check based on SHA1 hash itself

--- src/main/groovy/nl/patelski/examples/pwndpassword/PwndPassword.groovy
+++ src/main/groovy/nl/patelski/examples/pwndpassword/PwndPassword.groovy
@@ -12,10 +12,20 @@ class PwndPassword {
      * Check the given password.
      * @param password The password to check
      * @return true if the password is a known (leaked) password in Pwned Passwords' database
-     *         flase if the password is not known in Pwned Passwords' database
+     *         false if the password is not known in Pwned Passwords' database
      */
     Boolean check(String password) {
         String sha1 = password.digest('SHA-1').toUpperCase()
+        return checkBySHA1(sha1)
+    }
+
+    /**
+     * Check the given SHA1.
+     * @param sha1 The sha1 to check
+     * @return true if the sha1 is from a known (leaked) password in Pwned Passwords' database
+     *         false if the sha1 is from a password not in Pwned Passwords' database
+     */
+    Boolean checkBySHA1(String sha1) {
         println(sha1)
         String sha1start = sha1[0..4]
         String sha1end = sha1.substring(5)
