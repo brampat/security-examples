506e1163a147775874715ba07685ff0d26fb8b83
Bram Patelski <bram.patelski@gmail.com>
2019-07-09 13:37:24 +0200

    Adds Diceware (5xD6 rolls) as a password

--- src/main/groovy/nl/patelski/examples/password/complexity/utils/MaskSymbol.groovy
+++ src/main/groovy/nl/patelski/examples/password/complexity/utils/MaskSymbol.groovy
@@ -14,6 +14,7 @@ enum MaskSymbol {
     SYMBOL(33, "s"), //  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ (including space)
     ALPHA_SYMBOL(95L, "a"), // Alphanumeric or symbol, used by most password-generators
     UTF8(137000L, "8"), // The UTF8 characterset knows about 137k visible non-control (enter, backspace) characters
+    DICEWARE(7776L, "W"), // Diceware passwords, derived from 5x standard D6 dicerolls http://www.diceware.com/
     ENGLISH_WORDS(35000L, "E"), // The average person knows about 20-35k words. Source: https://www.economist.com/johnson/2013/05/29/lexical-facts
     DICTIONARY_WORDS(171476L, "D"), // The English dictionary holds about 171k words. Source: https://www.quora.com/How-many-words-are-there-in-the-English-language
     USA_CITIES(27000L, "c"), // Cities in the USA according to https://www.quora.com/How-many-towns-and-cities-are-there-in-the-world
